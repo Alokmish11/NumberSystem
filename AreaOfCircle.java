9a88243d454822173c56cdd55edb7a68267711a4
Alokmish11 <140834850+Alokmish11@users.noreply.github.com>
2024-05-12 21:42:02 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,12 @@
+public class AreaOfCircle {
+   public static void Area(int r){
+        double ans= 3.14*r*r;
+   System.out.print(ans);
+}
+public static void main(String[] args){
+    int r=5;
+    Area(r);
+}
+}
+    
+
